ef44934eed1ac3e281aef1bff3c54cafba191e50
Lev <lev@me.com>
2023-03-12 21:26:20 +0000

    Added Sync to USB function and working on RSS downloader

--- Download-Playlists.ps1
+++ Download-Playlists.ps1
@@ -13,124 +13,249 @@ function Install-ModuleIfNotPresent {
 	}
 }
 
+function Test-CommandExists {
+	param(
+		[string]$cmd
+	)
+
+	try {
+		Write-Debug "Checking for ${cmd}.."
+		&${cmd} 2>$NULL
+		Write-Debug "${cmd} found."
+		return $true
+	}
+	catch {
+		Write-Debug "${cmd} not found in %PATH%.."
+		return $false
+	}
+}
+
+
 Install-ModuleIfNotPresent 'PowerShell-YAML'
 
 
-$timestamp = `Get-Date -Format 'yyyyMMdd-HHmmss'` 
+$timestamp = Get-Date -Format 'yyyyMMdd-HHmmss'
 $archiveFileName = '_downloaded.txt'
 
-# $yamlFile = "AndrejKarpathy.yaml"
-$Yaml = Get-Content $YamlFile -Raw | ConvertFrom-Yaml | Select-Object -First 1
+$Yaml = Get-Content $YamlFile -Raw | ConvertFrom-Yaml
+$Playlists = $Yaml.Playlists
 
 $NamingStyles = @{
-	"playlist/date-title"  = "%(playlist)s/%(upload_date)s - %(title)s.%(ext)s"
-	"playlist/index-title" = "%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s"
-	"playlist/default"     = "%(playlist)s/%(title)s-%(id)s.%(ext)s"
-	"default"              = "%(title)s-%(id)s.%(ext)s"
-	"root/index-title"     = "%(playlist_index)s - %(title)s.%(ext)s"
-	"root/channel-title"   = "%(channel)s - %(title)s.%(ext)s"
+	"playlist/date-title" = "%(playlist)s/%(upload_date)s - %(title)s.%(ext)s"
+	"root/st/index-title" = "%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s"
+	"playlist/default"    = "%(playlist)s/%(title)s-%(id)s.%(ext)s" # Default
+	"default"             = "%(title)s-%(id)s.%(ext)s"
+	"root/index-title"    = "%(playlist_index)s - %(title)s.%(ext)s"
+	"root/channel-title"  = "%(channel)s - %(title)s.%(ext)s"
 }
 
-if (!$dlcmd) {
+if ($DlCmd) {
+	if (!(Test-CommandExists $DlCmd)) {
+		$Raise_Error = "ERROR: $DlCmd not found in %PATH%"; Throw $Raise_Error
+	}
+}
+else {
 	# Test yt-dlp
-	$dlcmd = "yt-dlp"
-	try {
-		Write-Debug "Checking for ${dlcmd}.."
-		&${dlcmd} 2>$NULL
-		Write-Debug "${dlcmd} found."
+	if (Test-CommandExists "yt-dlp") {
+		$DlCmd = "yt-dlp"
 	}
-	catch {
-		Write-Debug "${dlcmd} not found in %PATH%. Trying youtube-dl.."
-		$dlcmd = $null
-	}	
-}
-
-if (!$dlcmd) {
-	# Test Youtube-DL
-	$dlcmd = "youtube-dl"
-	try {
-		Write-Debug "Checking for youtube-dl.."
-		&youtube-dl 2>$NULL
-		Write-Debug "youtube-dl found."
+	elseif (Test-CommandExists "youtube-dl") {
+		$DlCmd = "youtube-dl"
 	}
-	catch {
-		$Raise_Error = "ERROR: Youtube-dl not found in %PATH%"; Throw $Raise_Error
+	else {
+		$Raise_Error = "ERROR: Neither `yt-dlp` nor `youtube-dl` found in %PATH%"; Throw $Raise_Error
 	}
 }
 
 # Test FFMPEG
-try {
-	Write-Debug "Checking for ffmpeg.."
-	&ffmpeg 2>$NULL
-	Write-Debug "ffmpeg found."
-}
-catch {
+if (!(Test-CommandExists "ffmpeg")) {
 	Write-Warning "WARNING: FFMPEG not found in %PATH%. Some features might not be available. Press ENTER to continue.."
 	Read-Host | Out-Null
 }
 
-foreach ($obj in $Yaml) {
-	Write-Debug "Processing: $($obj.folder)"
+foreach ($playlist in $Playlists) {
+	if ($playlist.stepTitle) {
+		$step = $playlist.stepTitle
+	}
+	else {
+		$step = $playlist.url
+	}
 
-	$folder = "${PSScriptRoot}\$($obj.folder)"
-	Write-Debug "Using folder: $folder"
+	Write-Debug "Processing: $step"
 
-	if (!(Test-Path $folder)) {
-		New-Item -ItemType Directory -Path $folder -Force -Verbose
+	$outPath = $playlist.outPath
+	Write-Debug "OutPath: $outPath"
+
+	Continue
+	if (!(Test-Path $outPath)) {
+		New-Item -ItemType Directory -Path $outPath -Force -Verbose
 	}
 	
-	Push-Location $folder
-	$archiveFile = "$folder\$archiveFileName"
-	$archiveOld = "$folder\_downloaded_old\"
-	foreach ($playlist in $obj.Playlists) {
-		Write-Debug "Processing playlist: $($playlist.name)"
-		# Backup old archive file
-		if (Test-Path  $archiveFile) {
-			if (!(Test-Path -PathType Container $archiveOld)) {
-				New-Item -ItemType Directory -Path $archiveOld -Force -Verbose
-			}
-			Copy-Item $archiveFile "$archiveOld/$((Get-Item $archiveFile).BaseName)-$timestamp$((Get-Item $archiveFile).Extension)" -Verbose
+	Push-Location $outPath
+	$archiveFile = "$outPath\$archiveFileName"
+	$archiveOldDir = "$folder\_downloaded_old\"
+	$archiveOldFile = "$archiveOldDir/$((Get-Item $archiveFile).BaseName)-$timestamp$((Get-Item $archiveFile).Extension)"
+
+	if (Test-Path  $archiveFile) {
+		if (!(Test-Path -PathType Container $archiveOldDir)) {
+			New-Item -ItemType Directory -Path $archiveOldDir -Force -Verbose
 		}
-	
-		if (!$playlist.naming) {
-			$playlist.naming = 'playlist/default'
-		}
-	
-		$Options = @(
-			'-v',
-			'-ciw',
-			'--download-archive', "`"$archiveFile`"",
-			'-o', "`"$($NamingStyles[$playlist.naming])`""
-			'--add-metadata'
-		)
-	
-		if ($playlist.naming) {
-			$Options += @(
-			)
-		}
-		else {
-			$Options += @(
-				'-o', "`"$($NamingStyles['playlist/default'])`""
-			)
-		}
-	
-		if ($playlist.options) {
-			$playlist.options.GetEnumerator() | ForEach-Object { $Options += "--$($_.Name)"; $Options += "$($_.Value)" }
-		}
-	
-		$Options = $Options |  Where-Object { $_ } # Remove empty elements from array
-		$Options += $playlist.url
-	
-		Write-Debug "Starting: $dlcmd $($Options -join ' ') "
-	
-		$process = Start-Process -FilePath $dlcmd -ArgumentList $Options -NoNewWindow -PassThru -Wait
-		if ($process.ExitCode -ne 0) {
-			Write-Error "Process exited with $($process.ExitCode)"
-			Start-Sleep 5
-		}    
+		Copy-Item $archiveFile $archiveOldFile -Verbose
 	}
 	
+	if (!$playlist.naming) {
+		# Default playlist naming style
+		$playlist.naming = 'playlist/default'
+	}
+	
+	# Default options
+	$Options = @(
+		'-v',
+		'-ciw',
+		'--download-archive', "`"$archiveFile`"",
+		'-o', "`"$($NamingStyles[$playlist.naming])`""
+		'--add-metadata'
+	)
+	
+	if ($playlist.options) {
+		$playlist.options.GetEnumerator() | ForEach-Object { $Options += "--$($_.Name)"; $Options += "$($_.Value)" }
+	}
+	
+	$Options = $Options |  Where-Object { $_ } # Remove empty elements from array
+	$Options += $playlist.url
+	
+	Write-Debug "Starting: $dlcmd $($Options -join ' ') "
+	
+	$process = Start-Process -FilePath $dlcmd -ArgumentList $Options -NoNewWindow -PassThru -Wait
+	if ($process.ExitCode -ne 0) {
+		Write-Error "Process exited with $($process.ExitCode)"
+		Start-Sleep 5
+	}
+
+	$aOldHash = Get-FileHash $archiveOldFile -Algorithm SHA256
+	$aNewHash = Get-FileHash $archiveFile -Algorithm SHA256
+	if ($aOldHash.Hash -eq $aNewHash.Hash) {
+		Write-Debug "Archive file unchanged. Removing backup archive file.."
+		Remove-Item $archiveOldFile -Force -Verbose
+	}
+
 	Pop-Location
 }
 
-Write-Debug "Done."
+WPop-Location
+r
+
+W
+
+WPop-Location
+r
+
+W
+
+WPop-Location
+r
+
+WPite-Debug "Done, all playlist entries processed."ite-Debug "Done, all playlist entries processed."rite-Debug "Done, all playlist entries processed."op-Location
+r
+
+Write-Debug "Done, all playlist entries processed."ite-Debug "Done, all playlist entries processed."rite-Debug "Done, all playlist entries processed."ite-Debug "Done, all playlist entries processed."
+
+# Test FFMPEG
+if (!(Test-CommandExists "ffmpeg")) {
+	Write-Warning "WARNING: FFMPEG not found in %PATH%. Some features might not be available. Press ENTER to continue.."
+	Read-Host | Out-Null
+}
+
+###
+###  Processing Youtube-DL playlists
+###
+
+foreach ($playlist in $Playlists) {
+	if ($playlist.stepTitle) {
+		$step = $playlist.stepTitle
+	}
+	else {
+		$step = $playlist.url
+	}
+
+	Write-Debug ">>> [PROCESSING]: $step"
+	Write-Debug ">>> [OutPath]: $outPath"
+
+	$outPath = $playlist.outPath
+
+	if (!(Test-Path $outPath)) {
+		New-Item -ItemType Directory -Path $outPath -Force -Verbose -ErrorAction Stop
+	}
+	
+	# Backup archive file
+	$archivePath = "$outPath\$archiveFileName"
+	$archiveFileBase = $archiveFileName -replace '\.[^.]+$', ''
+	$archiveFileExt = $archiveFileName -replace '^.*\.', ''
+	$archiveBackupDir = "$outPath\_downloaded_old\"
+	$archiveBackupPath = "${archiveBackupDir}/${archiveFileBase}-${timestamp}.${archiveFileExt}"
+
+	if (Test-Path  $archivePath) {
+		if (!(Test-Path -PathType Container $archiveBackupDir)) {
+			New-Item -ItemType Directory -Path $archiveBackupDir -Force
+		}
+		Copy-Item $archivePath $archiveBackupPath -Verbose
+	}
+	
+	if (!$playlist.naming) {
+		# Default playlist naming style
+		$playlist.naming = 'playlist/default'
+	}
+
+	if (!($NamingStyles[$playlist.naming])) {
+		$Raise_Error = "ERROR: Invalid naming style: $($playlist.naming)"; Throw $Raise_Error
+	}
+	
+	# Default options
+	$Options = @(
+		'-v',
+		'-ciw',
+		'--download-archive', "`"$archivePath`"",
+		'-o', "`"${outPath}/$($NamingStyles[$playlist.naming])`""
+		'--add-metadata'
+	)
+	
+	if ($playlist.options) {
+		$playlist.options.GetEnumerator() | ForEach-Object { $Options += "--$($_.Name)"; $Options += "$($_.Value)" }
+	}
+	
+	$Options = $Options |  Where-Object { $_ } # Remove empty elements from array
+	$Options += $playlist.url
+	
+	Write-Debug "Starting: $dlcmd $($Options -join ' ') "
+	
+	$process = Start-Process -FilePath $dlcmd -ArgumentList $Options -NoNewWindow -PassThru -Wait
+	if ($process.ExitCode -ne 0) {
+		Write-Error "Process exited with $($process.ExitCode)"
+		Start-Sleep 5
+	}
+
+	$aOldHash = Get-FileHash $archiveBackupPath -Algorithm SHA256
+	$aNewHash = Get-FileHash $archivePath -Algorithm SHA256
+	if ($aOldHash.Hash -eq $aNewHash.Hash) {
+		Write-Debug "Archive file unchanged. Removing backup archive file.."
+		Remove-Item $archiveBackupPath -Force -Verbose
+	}
+
+	###
+	###  Sync
+	###
+
+	if ($playlist.sync -eq $true ) {
+		$syncDrive = Get-SyncDevice $playlist.syncToDevice
+		$syncPath = "${syncDrive}$($playlist.syncRoot)"
+
+		Invoke-FileSync -Source $outPath -Destination $syncPath
+	}
+}
+Write-Debug "Done, all playlist entries processed."
+
+$RssFeeds = $Yaml.RssFeeds
+
+foreach ($rss in $RssFeeds) {
+	
+}
---
+++ DownloadRss.ps1
@@ -0,0 +1,92 @@
+$url = "https://rss.art19.com/how-i-built-this"
+
+# Set the directory where the downloaded episodes will be saved
+$directory = "D:\Temp\Podcasts"
+
+$archiveFile = "D:\Temp\Podcasts\downloaded.csv"
+
+
+# Create the directory if it doesn't already exist
+if (!(Test-Path $directory)) {
+  New-Item -ItemType Directory -Path $directory | Out-Null
+}
+
+$client = New-Object System.Net.WebClient
+
+# Download the RSS feed and parse it as XML
+[xml]$rss = $client.DownloadString($url)
+
+function Get-UrlHash {
+  param(
+    [string]$Url
+  )
+  $urlBase = $Url -replace '\?.*$', ''
+  $urlHashBin = [System.Security.Cryptography.MD5]::Create().ComputeHash([System.Text.Encoding]::UTF8.GetBytes($urlBase))
+  $urlHash = [System.BitConverter]::ToString($urlHashBin).Replace('-', '').ToLower()
+  return $urlHash  
+}
+
+function Check-ArchiveFile {
+  param(
+    [string]$ArchiveFile,
+    [string]$Url
+  )
+  ### Format:
+  # UrlHash,Title
+
+  if (!(Test-Path $ArchiveFile)) {
+    return $false
+  }
+
+  $archive = Get-Content $ArchiveFile | ConvertFrom-Csv
+
+  return $archive.UrlHash -contains $(Get-UrlHash($Url))
+}
+
+function Record-ArchiveFile {
+  param(
+    [string]$ArchiveFile,
+    [string]$Url,
+    [string]$Title
+  )
+  
+  if (!(Test-Path $ArchiveFile)) {
+    New-Item -ItemType File -Path $ArchiveFile | Out-Null
+    # Add CSV headers
+    Add-Content $ArchiveFile "UrlHash,Title"
+  }
+
+  $urlHash = Get-UrlHash($Url)
+  Add-Content $ArchiveFile "${urlHash},${Title}"
+}
+
+# Loop through each item in the RSS feed
+foreach ($item in $rss.rss.channel.item) {
+  $mp3Url = $item.enclosure.url
+  $title = $item.title[0]
+
+  $isArchive = Check-ArchiveFile -ArchiveFile $archiveFile -Url $mp3Url
+  if ($isArchive) {
+    Write-Debug "$title is already downloaded, sipping."
+    Continue
+  }
+
+  $filename = "$($item.episode). $title.mp3.part" -replace '[\\/:*?"<>|]', '' # Remove the special characters
+
+  Write-Debug "Downloading $mp3Url..."
+  Write-Debug "Title: $title"
+
+  $filepath = "$directory\$filename"
+  $client.DownloadFile($mp3Url, $filepath)
+  # Check if download was successful
+  if ($client.ResponseHeaders['Content-Length'] -eq (Get-Item $filepath).Length) {
+    # Rename the file to remove the .part extension
+    Rename-Item -Path $filepath -NewName ($filepath -replace '\.part$', '') -Force | Out-Null
+    Write-Debug "COMPLETED: $title"
+    Record-ArchiveFile -ArchiveFile $archiveFile -Url $mp3Url -Title $title
+  }
+  else {
+    Write-Error "Download failed for $title"
+  }
+}
+
